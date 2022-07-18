c2ed574a12f90dfe75c10b0a6f97bd7038eacf19
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2022-07-19 00:48:55 +0530

    Add files via upload

---
+++ TestProject.java
@@ -0,0 +1,368 @@
+package test_project;
+
+public class TestProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		int a=15;
+//		System.out.println(a);
+		
+		
+		
+//		float ba=15.55f;
+//		System.out.println(ba);
+		
+		
+		
+//		boolean ab=true;
+//		System.out.println(ab);
+		
+		
+		
+//		String aa="Mal";
+//		System.out.println(aa);
+		
+		
+		
+//		if(20>15) {
+//			System.out.println("20 is greater than 15");
+//			}
+//			else {
+//				System.out.println("15 is lower than 20");
+//			}
+		
+		
+		
+//		if(20<15) {
+//		System.out.println("20 is greater than 15");
+//		}
+//		else {
+//			System.out.println("15 is lower than 20");
+//		}
+		
+		
+		
+//		int x= 20;
+//		int y =15;
+//		
+//		if(y>x) {
+//			System.out.println("y is greater than x");
+//		}else {
+//				System.out.println("x is greater than y");
+//	
+//		}
+		
+		
+//		int marks=65;
+//		
+//		if(marks<50) {
+//			System.out.println("Fail");
+//		}
+//		else if(marks>=50 && marks<60) {
+//			System.out.println("C pass");
+//		}
+//		else if(marks>=60 && marks<65) {
+//			System.out.println("B pass");
+//		}
+//		else {
+//			System.out.println("A pass");
+//		}
+		
+		
+//		int num=-50;
+//		
+//		if(num>0) {
+//			System.out.println("Positive");
+//		}
+//		else if(num<0) {
+//			System.out.println("Negative");
+//		}
+//		else {
+//			System.out.println("Zero");
+//		}
+		
+		
+//	int num =0;
+//	
+//	while(num<=5) {
+//		System.out.println(num);
+//		++num;
+//	}
+		
+//		for(int i=5; i<=20;i=i+5) {
+//			System.out.println(i);
+//		}
+		
+		
+//		int num=0;
+//		
+//		while(num<=5) {
+//			System.out.println(num);
+//			++num;
+//		}
+		
+		
+	
+//		for(int num=5; num <=20 ; num=num+5  ) {
+//			System.out.println(num);
+//		}
+		
+		
+//		int row=5;	
+//		for(int i=0; i<row;i++)
+//		{
+//		for(int j=0; j<=i; j++) 
+//		{
+//		System.out.print("* ");
+//		}
+//		System.out.println();
+//		}
+		
+		
+ 
+//		int row=6;    
+//		for(int i=0; i<row; i++)   
+//		{   
+//		for(int j=0; j<=i; j++)   
+//		{   
+//		System.out.print("* ");   
+//		}   
+//		System.out.println();   
+//		}   
+		 
+		
+//		int a=10;
+//		int b=20;
+		
+//		System.out.println(a+b);
+//		System.out.println(a-b);
+//		System.out.println(a*b);
+//		System.out.println(a/b);
+		
+//		float a=10f;
+//		float b=20f;
+//		System.out.println(a/b);
+		
+	//	relational operators
+		
+//		int a=10;
+//		int b=20;
+//		
+//		System.out.println(a<b);//output is true
+//		System.out.println(a>b);//output is false
+//		System.out.println(a==b);//output is false
+//		System.out.println(a!=b);//output is true
+		
+		
+		
+//		boolean a=true;
+//		boolean b=false;
+//		
+//		System.out.println(a && b);//output is false
+//		System.out.println(b && a);//output is false
+//		System.out.println(b && b);//output is false
+//		System.out.println(a && a);//output is true
+//		System.out.println(a || b);//output is true
+//		System.out.println(b || a);//output is true
+//		System.out.println(a || a);//output is true
+//		System.out.println(b || b);//output is false
+		
+		
+//		char[] a = new char[5];
+//		
+//		a[0] = 'a';
+//		a[1] = 'b';
+//		a[2] = 'c';
+//		a[3] = 'd';
+//		a[4] = 'e';
+//		
+//		for(int i=0; i<5; i++) {
+//		System.out.println(a[i]);
+//		}
+		
+//		
+//		int[] a = new int[10];
+//		for(int i=0; i<10; i++) {
+//		a[i] = i;
+//	}
+//		
+//		for(int i=0; i<10; i++) {
+//			System.out.println(a[i]);
+//		}
+		
+		
+//		int a[]= new int[5];
+//		int b[]= new int[5];
+//		int c[]= new int[5];
+//		
+//		int num = 5;
+//		
+//		for(int i=0; i<5; i++) {
+//			a[i]= i;
+//		}
+//		
+//		for(int i=0; i<5; i++) {
+//			b[i] = num;
+//			++num;
+//		}
+//		
+//		System.out.print("Values of array 'A': ");
+//		for(int i=0; i<5; i++) {
+//			System.out.print(a[i]+ " ");
+//		}
+//		
+//		System.out.print("\nValues of array 'B': ");
+//		for(int i=0; i<5; i++) {
+//			System.out.print(b[i]+ " ");
+//		}
+//		
+//		
+//		for(int i=0; i<5; i++) {
+//			c[i] = a[i] + b[i];
+//		}
+//		System.out.print("\nValues of array 'C': ");
+//		for(int i=0; i<5; i++) {
+//			System.out.print(c[i]+ " ");
+//		}
+		
+		//OUTPUT
+		
+//		Values of array 'A': 0 1 2 3 4 
+//		Values of array 'B': 5 6 7 8 9 
+//		Values of array 'C': 5 7 9 11 13 
+
+	
+		//MULTI-DIMENSIONAL ARRAY
+		
+//		int a[][] = new int[3][3];
+//		
+//		int num =0;
+//		
+//		for(int i=0; i<3; i++) {
+//			for(int j=0; j<3; j++) {
+//				a[i][j] = num;
+//				++num;
+//			}
+//		}
+//		
+//		for(int i=0; i<3; i++) {
+//			for(int j=0; j<3; j++) {
+//				System.out.print(a[i][j]+ " ");
+//			}
+//			System.out.println();
+//		}
+//		
+	
+		
+		//FUNCTION
+		
+		
+//		sparta();
+//		
+//}
+//	
+//	public static void sparta() {
+//		System.out.println("This is spartaaaa!");
+//	}
+		
+//		int a = 10;
+//		int b = 20;
+//		
+//		add(a,b);
+//	}
+//	
+//	public static void add(int a, int b) {
+//		System.out.println(a+b);
+//	}
+	
+		
+		
+//		TestProject obj = new TestProject();
+//		
+//		int x = 100;
+//		int y = 200;
+//		
+//		int get_result = obj.add(x,y);
+//		
+//		System.out.println(get_result);
+//	}
+//	
+//	public int add(int a, int b) {
+//		
+//		int result = a+b;
+//		return result;
+//	}
+	
+		
+		//SWAPPING VALUES
+		
+		
+//		int x = 10;
+//		int y = 20;
+//		
+//		swapFunction(x,y);
+//	}
+//	
+//	
+//	public static void swapFunction(int a, int b) {
+//		System.out.println("Original values a:"+a+" b:" +b);
+//		
+//		int c=a;
+//		a=b;
+//		b=c;
+//		
+//		System.out.println("Original values a:"+a+" b:" +b);
+//	}
+	
+	
+//	int x=10;
+//	int y=20;
+//	
+//	int r = 5;
+//	
+//	TestProject obj = new TestProject();
+//	
+//	float area_circle = obj.area(r);
+//	
+//	int area_rectangle = obj.area(x, y);
+//	
+//	System.out.println("Area of the circle is:" +area_circle);
+//	
+//	System.out.println("Area of the rectangle is:" +area_rectangle);
+//	}
+//	
+//	public int area(int n1, int n2) {
+//		
+//		int result = n1*n2;
+//		return result;
+//	}
+//	
+//	public float area(int n) {
+//		float result = 3.14f*n*n;
+//		return result;
+//	}
+//	
+	
+//		   int[] arr = new int[2];
+//
+//	        arr[0] = 10;
+//
+//	        arr[1] = 20;
+//
+//	  
+//
+//	        for (int i = 0; i <= arr.length; i++)
+//
+//	            System.out.println(arr[i]);
+//
+//	    }
+	
+	
+		int val1 = 10;
+
+		int val2 = 20;
+
+		int max = val1 >= val2 ? val1 : val2;
+	}
+}
